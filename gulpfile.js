b0cced520f8c0608040f9b13e474babf74772709
Troy Denney <tvdenney@gmail.com>
2016-10-06 14:03:00 -0400

    more set up

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function () {
+  return gulp.src('./css/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./css/**/*.scss', ['default']);
+});
